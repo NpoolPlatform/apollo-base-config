068ce7fe5d416c0ab704927b7d4e281eba1faccf
hong-t <th13564039482@163.com>
2021-10-29 22:34:37 +0800

    add apollo base config

---
+++ apollo-appid-config.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+APP_ID=$1
+PASSWORD=$2
+CLUSTERNAME=$3
+ENVIRONMENT=`echo $CLUSTERNAME | tr a-z A-Z`
+
+kubectl -n kube-system exec mysql-0 -- mysql -h 127.0.0.1 -uroot -p$PASSWORD -P3306 -D ApolloConfigDB -e "INSERT INTO App (AppId, Name, OrgId, OrgName, OwnerName, OwnerEmail,DataChange_CreatedBy, DataChange_LastModifiedBy) SELECT $APP_ID, \"test\", \"TEST1\", \"npool\", \"apollo\", \"apollo@acme.com\", \"apollo\", \"apollo\" FROM DUAL WHERE NOT EXISTS (SELECT * FROM App WHERE AppId=\"$APP_ID\");"
+
+kubectl -n kube-system exec mysql-0 -- mysql -h 127.0.0.1 -uroot -p$PASSWORD -P3306 -D ApolloConfigDB -e "INSERT INTO Cluster (Name, AppId, DataChange_CreatedBy, DataChange_LastModifiedBy) SELECT \"$CLUSTERNAME\", $APP_ID, \"apollo\", \"apollo\" FROM DUAL WHERE NOT EXISTS (SELECT * FROM Cluster WHERE AppId="$APP_ID" AND Name=\"$CLUSTERNAME\");"
+
+kubectl -n kube-system exec mysql-0 -- mysql -h 127.0.0.1 -uroot -p$PASSWORD -P3306 -D ApolloPortalDB -e "INSERT INTO App (AppId, Name, OrgId, OrgName, OwnerName, OwnerEmail,DataChange_CreatedBy, DataChange_LastModifiedBy) SELECT $APP_ID, \"test\", \"TEST1\", \"npool\", \"apollo\", \"apollo@acme.com\", \"apollo\", \"apollo\" FROM DUAL WHERE NOT EXISTS (SELECT * FROM App WHERE AppId=\"$APP_ID\");"
+
+kubectl -n kube-system exec mysql-0 -- mysql -h 127.0.0.1 -uroot -p$PASSWORD -P3306 -D ApolloPortalDB -e "INSERT INTO Permission (PermissionType, TargetId, DataChange_CreatedBy, DataChange_LastModifiedBy) SELECT \"CreateCluster\", $APP_ID, \"apollo\", \"apollo\" FROM DUAL WHERE NOT EXISTS (SELECT * FROM Permission WHERE TargetId=\"$APP_ID\" AND PermissionType=\"CreateCluster\");"
+kubectl -n kube-system exec mysql-0 -- mysql -h 127.0.0.1 -uroot -p$PASSWORD -P3306 -D ApolloPortalDB -e "INSERT INTO Permission (PermissionType, TargetId, DataChange_CreatedBy, DataChange_LastModifiedBy) SELECT \"CreateNamespace\", $APP_ID, \"apollo\", \"apollo\" FROM DUAL WHERE NOT EXISTS (SELECT * FROM Permission WHERE TargetId=\"$APP_ID\" AND PermissionType=\"CreateNamespace\");"
+kubectl -n kube-system exec mysql-0 -- mysql -h 127.0.0.1 -uroot -p$PASSWORD -P3306 -D ApolloPortalDB -e "INSERT INTO Permission (PermissionType, TargetId, DataChange_CreatedBy, DataChange_LastModifiedBy) SELECT \"AssignRole\", $APP_ID, \"apollo\", \"apollo\" FROM DUAL WHERE NOT EXISTS (SELECT * FROM Permission WHERE TargetId=\"$APP_ID\" AND PermissionType=\"AssignRole\");"
+kubectl -n kube-system exec mysql-0 -- mysql -h 127.0.0.1 -uroot -p$PASSWORD -P3306 -D ApolloPortalDB -e "INSERT INTO Permission (PermissionType, TargetId, DataChange_CreatedBy, DataChange_LastModifiedBy) SELECT \"ManageAppMaster\", $APP_ID, \"apollo\", \"apollo\" FROM DUAL WHERE NOT EXISTS (SELECT * FROM Permission WHERE TargetId=\"$APP_ID\" AND PermissionType=\"ManageAppMaster\");"
+
+kubectl -n kube-system exec mysql-0 -- mysql -h 127.0.0.1 -uroot -p$PASSWORD -P3306 -D ApolloPortalDB -e "INSERT INTO Role (RoleName, DataChange_CreatedBy, DataChange_LastModifiedBy) SELECT \"Master+$APP_ID\", \"apollo\", \"apollo\" FROM DUAL WHERE NOT EXISTS (SELECT * FROM Role WHERE RoleName=\"Master+$APP_ID\");"
+kubectl -n kube-system exec mysql-0 -- mysql -h 127.0.0.1 -uroot -p$PASSWORD -P3306 -D ApolloPortalDB -e "INSERT INTO Role (RoleName, DataChange_CreatedBy, DataChange_LastModifiedBy) SELECT \"ManageAppMaster+$APP_ID\", \"apollo\", \"apollo\" FROM DUAL WHERE NOT EXISTS (SELECT * FROM Role WHERE RoleName=\"ManageAppMaster+$APP_ID\");"
+
+
+master=`kubectl -n kube-system exec mysql-0 -- mysql -h 127.0.0.1 -uroot -p$PASSWORD -P3306 -D ApolloPortalDB -e "select Id from Role where RoleName=\"Master+$APP_ID\";"`
+masterroleid=`echo $master | awk '{ print $2 }'`
+masterpermission=`kubectl -n kube-system exec mysql-0 -- mysql -h 127.0.0.1 -uroot -p$PASSWORD -P3306 -D ApolloPortalDB -e "select Id from Permission where (PermissionType=\"AssignRole\" or PermissionType=\"CreateCluster\" or PermissionType=\"CreateNamespace\") and TargetId=\"$APP_ID\";"`
+masterpermissionid=`echo $masterpermission | awk -F 'Id ' '{ print $2 }'`
+for permissionid in $masterpermissionid;do
+  kubectl -n kube-system exec mysql-0 -- mysql -h 127.0.0.1 -uroot -p$PASSWORD -P3306 -D ApolloPortalDB -e "INSERT INTO RolePermission (RoleId, PermissionId, DataChange_CreatedBy, DataChange_LastModifiedBy) SELECT $masterroleid, $permissionid, \"apollo\", \"apollo\" FROM DUAL WHERE NOT EXISTS (SELECT * FROM RolePermission WHERE RoleId=\"$masterroleid\" AND PermissionId=\"$permissionid\");"
+done
+
+manageappmaster=`kubectl -n kube-system exec mysql-0 -- mysql -h 127.0.0.1 -uroot -p$PASSWORD -P3306 -D ApolloPortalDB -e "select Id from Role where RoleName=\"ManageAppMaster+$APP_ID\";"`
+manageroleid=`echo $manageappmaster | awk '{ print $2 }'`
+managerpermission=`kubectl -n kube-system exec mysql-0 -- mysql -h 127.0.0.1 -uroot -p$PASSWORD -P3306 -D ApolloPortalDB -e "select Id from Permission where PermissionType=\"ManageAppMaster\" and TargetId=\"$APP_ID\";"`
+managerpermissionid=`echo $managerpermission | awk -F 'Id ' '{ print $2 }'`
+for permissionid in $managerpermissionid;do
+  kubectl -n kube-system exec mysql-0 -- mysql -h 127.0.0.1 -uroot -p$PASSWORD -P3306 -D ApolloPortalDB -e "INSERT INTO RolePermission (RoleId, PermissionId, DataChange_CreatedBy, DataChange_LastModifiedBy) SELECT $manageroleid, $permissionid, \"apollo\", \"apollo\" FROM DUAL WHERE NOT EXISTS (SELECT * FROM RolePermission WHERE RoleId=\"$manageroleid\" AND PermissionId=\"$permissionid\");"
+done
---
+++ apollo-base-config.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+APP_ID=$1
+PASSWORD=$2
+CLUSTERNAME=$3
+ENVIRONMENT=`echo $CLUSTERNAME | tr a-z A-Z`
+APP_HOST=$4
+
+kubectl -n kube-system exec mysql-0 -- mysql -h 127.0.0.1 -uroot -p$PASSWORD -P3306 -D ApolloConfigDB -e "INSERT INTO AppNamespace (Name, AppId, Comment, DataChange_CreatedBy, DataChange_LastModifiedBy) SELECT \"$APP_HOST\", $APP_ID, \"$APP_HOST namespace\", \"apollo\", \"apollo\" FROM DUAL WHERE NOT EXISTS (SELECT * FROM AppNamespace WHERE AppId=\"$APP_ID\" AND Name=\"$APP_HOST\");"
+
+kubectl -n kube-system exec mysql-0 -- mysql -h 127.0.0.1 -uroot -p$PASSWORD -P3306 -D ApolloConfigDB -e "INSERT INTO Namespace (AppId, ClusterName, NamespaceName, DataChange_CreatedBy, DataChange_LastModifiedBy) SELECT $APP_ID, \"$CLUSTERNAME\", \"$APP_HOST\", \"apollo\", \"apollo\" FROM DUAL WHERE NOT EXISTS (SELECT * FROM Namespace WHERE AppId=\"$APP_ID\" AND ClusterName=\"$CLUSTERNAME\" and NamespaceName=\"$APP_HOST\");"
+
+kubectl -n kube-system exec mysql-0 -- mysql -h 127.0.0.1 -uroot -p$PASSWORD -P3306 -D ApolloPortalDB -e "INSERT INTO AppNamespace (Name, AppId, Comment, DataChange_CreatedBy, DataChange_LastModifiedBy) SELECT \"$APP_HOST\", $APP_ID, \"$APP_HOST namespace\", \"apollo\", \"apollo\" FROM DUAL WHERE NOT EXISTS (SELECT * FROM AppNamespace WHERE AppId=\"$APP_ID\" AND Name=\"$APP_HOST\");"
+
+kubectl -n kube-system exec mysql-0 -- mysql -h 127.0.0.1 -uroot -p$PASSWORD -P3306 -D ApolloPortalDB -e "INSERT INTO Permission (PermissionType, TargetId, DataChange_CreatedBy, DataChange_LastModifiedBy) SELECT \"ModifyNamespace\", \"$APP_ID+$APP_HOST\", \"apollo\", \"apollo\" FROM DUAL WHERE NOT EXISTS (SELECT * FROM Permission WHERE TargetId=\"$APP_ID+$APP_HOST\" AND PermissionType=\"ModifyNamespace\");"
+
+kubectl -n kube-system exec mysql-0 -- mysql -h 127.0.0.1 -uroot -p$PASSWORD -P3306 -D ApolloPortalDB -e "INSERT INTO Permission (PermissionType, TargetId, DataChange_CreatedBy, DataChange_LastModifiedBy) SELECT \"ReleaseNamespace\", \"$APP_ID+$APP_HOST\", \"apollo\", \"apollo\" FROM DUAL WHERE NOT EXISTS (SELECT * FROM Permission WHERE TargetId=\"$APP_ID+$APP_HOST\" AND PermissionType=\"ReleaseNamespace\");"
+
+kubectl -n kube-system exec mysql-0 -- mysql -h 127.0.0.1 -uroot -p$PASSWORD -P3306 -D ApolloPortalDB -e "INSERT INTO Permission (PermissionType, TargetId, DataChange_CreatedBy, DataChange_LastModifiedBy) SELECT \"ModifyNamespace\", \"$APP_ID+$APP_HOST+$ENVIRONMENT\", \"apollo\", \"apollo\" FROM DUAL WHERE NOT EXISTS (SELECT * FROM Permission WHERE TargetId=\"$APP_ID+$APP_HOST+$ENVIRONMENT\" AND PermissionType=\"ModifyNamespace\");"
+
+kubectl -n kube-system exec mysql-0 -- mysql -h 127.0.0.1 -uroot -p$PASSWORD -P3306 -D ApolloPortalDB -e "INSERT INTO Permission (PermissionType, TargetId, DataChange_CreatedBy, DataChange_LastModifiedBy) SELECT \"ReleaseNamespace\", \"$APP_ID+$APP_HOST+$ENVIRONMENT\", \"apollo\", \"apollo\" FROM DUAL WHERE NOT EXISTS (SELECT * FROM Permission WHERE TargetId=\"$APP_ID+$APP_HOST+$ENVIRONMENT\" AND PermissionType=\"ReleaseNamespace\");"
+
+kubectl -n kube-system exec mysql-0 -- mysql -h 127.0.0.1 -uroot -p$PASSWORD -P3306 -D ApolloPortalDB -e "INSERT INTO Role (RoleName, DataChange_CreatedBy, DataChange_LastModifiedBy) SELECT \"ModifyNamespace+$APP_ID+$APP_HOST\", \"apollo\", \"apollo\" FROM DUAL WHERE NOT EXISTS (SELECT * FROM Role WHERE RoleName=\"ModifyNamespace+$APP_ID+$APP_HOST\");"
+
+kubectl -n kube-system exec mysql-0 -- mysql -h 127.0.0.1 -uroot -p$PASSWORD -P3306 -D ApolloPortalDB -e "INSERT INTO Role (RoleName, DataChange_CreatedBy, DataChange_LastModifiedBy) SELECT \"ReleaseNamespace+$APP_ID+$APP_HOST\", \"apollo\", \"apollo\" FROM DUAL WHERE NOT EXISTS (SELECT * FROM Role WHERE RoleName=\"ReleaseNamespace+$APP_ID+$APP_HOST\");"
+
+kubectl -n kube-system exec mysql-0 -- mysql -h 127.0.0.1 -uroot -p$PASSWORD -P3306 -D ApolloPortalDB -e "INSERT INTO Role (RoleName, DataChange_CreatedBy, DataChange_LastModifiedBy) SELECT \"ModifyNamespace+$APP_ID+$APP_HOST+$ENVIRONMENT\", \"apollo\", \"apollo\" FROM DUAL WHERE NOT EXISTS (SELECT * FROM Role WHERE RoleName=\"ModifyNamespace+$APP_ID+$APP_HOST+$ENVIRONMENT\");"
+
+kubectl -n kube-system exec mysql-0 -- mysql -h 127.0.0.1 -uroot -p$PASSWORD -P3306 -D ApolloPortalDB -e "INSERT INTO Role (RoleName, DataChange_CreatedBy, DataChange_LastModifiedBy) SELECT \"ReleaseNamespace+$APP_ID+$APP_HOST+$ENVIRONMENT\", \"apollo\", \"apollo\" FROM DUAL WHERE NOT EXISTS (SELECT * FROM Role WHERE RoleName=\"ReleaseNamespace+$APP_ID+$APP_HOST+$ENVIRONMENT\");"
+
+id=`kubectl -n kube-system exec mysql-0 -- mysql -h 127.0.0.1 -uroot -p$PASSWORD -P3306 -D ApolloConfigDB -e "select Id from Namespace where NamespaceName=\"$APP_HOST\";"`
+appnamespaceid=`echo $id | awk '{ print $2 }'`
+
+# insert in Jenkinsfile
+# kubectl -n kube-system exec mysql-0 -- mysql -h 127.0.0.1 -uroot -p$PASSWORD -P3306 -D ApolloConfigDB -e "INSERT INTO Item (NamespaceId, \`Key\`, Value, LineNum, DataChange_CreatedBy, DataChange_LastModifiedBy) SELECT $appnamespaceid, \"username\", \"root\", 1, \"apollo\", \"apollo\" FROM DUAL WHERE NOT EXISTS (SELECT * FROM Item WHERE NamespaceId=\"$appnamespaceid\" AND \`Key\`=\"username\");"
+# kubectl -n kube-system exec mysql-0 -- mysql -h 127.0.0.1 -uroot -p$PASSWORD -P3306 -D ApolloConfigDB -e "INSERT INTO Item (NamespaceId, \`Key\`, Value, LineNum, DataChange_CreatedBy, DataChange_LastModifiedBy) SELECT $appnamespaceid, \"password\", \"$PASSWORD\", 2, \"apollo\", \"apollo\" FROM DUAL WHERE NOT EXISTS (SELECT * FROM Item WHERE NamespaceId=\"$appnamespaceid\" AND \`Key\`=\"password\");"
+
+modifyrole=`kubectl -n kube-system exec mysql-0 -- mysql -h 127.0.0.1 -uroot -p$PASSWORD -P3306 -D ApolloPortalDB -e "select Id from Role where RoleName=\"ModifyNamespace+$APP_ID+$APP_HOST\";"`
+modifyroleid=`echo $modifyrole | awk '{ print $2 }'`
+modifyrolepermission=`kubectl -n kube-system exec mysql-0 -- mysql -h 127.0.0.1 -uroot -p$PASSWORD -P3306 -D ApolloPortalDB -e "select Id from Permission where PermissionType=\"ModifyNamespace\" and TargetId=\"$APP_ID+$APP_HOST\";"`
+modifyrolepermissionid=`echo $modifyrolepermission | awk -F 'Id ' '{ print $2 }'`
+for permissionid in $modifyrolepermissionid;do
+  kubectl -n kube-system exec mysql-0 -- mysql -h 127.0.0.1 -uroot -p$PASSWORD -P3306 -D ApolloPortalDB -e "INSERT INTO RolePermission (RoleId, PermissionId, DataChange_CreatedBy, DataChange_LastModifiedBy) SELECT $modifyroleid, $permissionid, \"apollo\", \"apollo\" FROM DUAL WHERE NOT EXISTS (SELECT * FROM RolePermission WHERE RoleId=\"$modifyroleid\" AND PermissionId=\"$permissionid\");"
+done
+ 
+releaserole=`kubectl -n kube-system exec mysql-0 -- mysql -h 127.0.0.1 -uroot -p$PASSWORD -P3306 -D ApolloPortalDB -e "select Id from Role where RoleName=\"ReleaseNamespace+$APP_ID+$APP_HOST\";"`
+releaseroleid=`echo $releaserole | awk '{ print $2 }'`
+releaserolepermission=`kubectl -n kube-system exec mysql-0 -- mysql -h 127.0.0.1 -uroot -p$PASSWORD -P3306 -D ApolloPortalDB -e "select Id from Permission where PermissionType=\"ReleaseNamespace\" and TargetId=\"$APP_ID+$APP_HOST\";"`
+releaserolepermissionid=`echo $releaserolepermission | awk -F 'Id ' '{ print $2 }'`
+for permissionid in $releaserolepermissionid;do
+  kubectl -n kube-system exec mysql-0 -- mysql -h 127.0.0.1 -uroot -p$PASSWORD -P3306 -D ApolloPortalDB -e "INSERT INTO RolePermission (RoleId, PermissionId, DataChange_CreatedBy, DataChange_LastModifiedBy) SELECT $releaseroleid, $permissionid, \"apollo\", \"apollo\" FROM DUAL WHERE NOT EXISTS (SELECT * FROM RolePermission WHERE RoleId=\"$releaseroleid\" AND PermissionId=\"$permissionid\");"
+done
+
+modifyrole_env=`kubectl -n kube-system exec mysql-0 -- mysql -h 127.0.0.1 -uroot -p$PASSWORD -P3306 -D ApolloPortalDB -e "select Id from Role where RoleName=\"ModifyNamespace+$APP_ID+$APP_HOST+$ENVIRONMENT\";"`
+modifyroleid_env=`echo $modifyrole_env | awk '{ print $2 }'`
+modifyrolepermission_env=`kubectl -n kube-system exec mysql-0 -- mysql -h 127.0.0.1 -uroot -p$PASSWORD -P3306 -D ApolloPortalDB -e "select Id from Permission where PermissionType=\"ModifyNamespace\" and TargetId=\"$APP_ID+$APP_HOST+$ENVIRONMENT\";"`
+modifyrolepermissionid_env=`echo $modifyrolepermission_env | awk -F 'Id ' '{ print $2 }'`
+for permissionid in $modifyrolepermissionid_env;do
+  kubectl -n kube-system exec mysql-0 -- mysql -h 127.0.0.1 -uroot -p$PASSWORD -P3306 -D ApolloPortalDB -e "INSERT INTO RolePermission (RoleId, PermissionId, DataChange_CreatedBy, DataChange_LastModifiedBy) SELECT $modifyroleid_env, $permissionid, \"apollo\", \"apollo\" FROM DUAL WHERE NOT EXISTS (SELECT * FROM RolePermission WHERE RoleId=\"$modifyroleid_env\" AND PermissionId=\"$permissionid\");"
+done
+
+releaserole_env=`kubectl -n kube-system exec mysql-0 -- mysql -h 127.0.0.1 -uroot -p$PASSWORD -P3306 -D ApolloPortalDB -e "select Id from Role where RoleName=\"ReleaseNamespace+$APP_ID+$APP_HOST+$ENVIRONMENT\";"`
+releaseroleid_env=`echo $releaserole_env | awk '{ print $2 }'`
+releaserolepermission_env=`kubectl -n kube-system exec mysql-0 -- mysql -h 127.0.0.1 -uroot -p$PASSWORD -P3306 -D ApolloPortalDB -e "select Id from Permission where PermissionType=\"ReleaseNamespace\" and TargetId=\"$APP_ID+$APP_HOST+$ENVIRONMENT\";"`
+releaserolepermissionid_env=`echo $releaserolepermission_env | awk -F 'Id ' '{ print $2 }'`
+for permissionid in $releaserolepermissionid_env;do
+  kubectl -n kube-system exec mysql-0 -- mysql -h 127.0.0.1 -uroot -p$PASSWORD -P3306 -D ApolloPortalDB -e "INSERT INTO RolePermission (RoleId, PermissionId, DataChange_CreatedBy, DataChange_LastModifiedBy) SELECT $releaseroleid_env, $permissionid, \"apollo\", \"apollo\" FROM DUAL WHERE NOT EXISTS (SELECT * FROM RolePermission WHERE RoleId=\"$releaseroleid_env\" AND PermissionId=\"$permissionid\");"
+done
